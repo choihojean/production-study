d22724f671d729a6289de7b0d6c15af7e441c02d
choihojean <chlghwls1231@naver.com>
2025-12-14 15:51:41 +0900

    feat: product페이지 리다이렉션 변경

--- app/features/products/pages/products-page.tsx
+++ app/features/products/pages/products-page.tsx
@@ -1,26 +1,5 @@
-import type { MetaFunction } from "react-router";
-import type { Route } from "./+types/products-page";
+import { redirect } from "react-router";
 
-export function meta({}: Route.MetaArgs): ReturnType<MetaFunction> {
-	return [
-		{ title: "Products | wemake" },
-		{ name: "description", content: "Browse all products" },
-	];
+export function loader() {
+	return redirect("/products/leaderboards");
 }
-
-export function loader({}: Route.LoaderArgs) {
-	return {};
-}
-
-export function action({}: Route.ActionArgs) {
-	return {};
-}
-
-export default function ProductsPage() {
-	return (
-		<div>
-			<h1>Products</h1>
-		</div>
-	);
-}
-
